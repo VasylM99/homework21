c535af589067d7f76f8bb4a3420fda11d4e22481
Vasyl <muran.wasyl@ukr.net>
2020-04-29 13:22:44 +0300

    Updates

---
+++ precache-manifest.f3049ffc3c62126fde76b208499bc544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a959efe161859ce42869360e718c742",
+    "url": "/homework21/index.html"
+  },
+  {
+    "revision": "4c1bbab9e64da0537456",
+    "url": "/homework21/static/css/main.98b24718.chunk.css"
+  },
+  {
+    "revision": "7c8ecd441ad62f7463fb",
+    "url": "/homework21/static/js/2.77b8deaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework21/static/js/2.77b8deaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1bbab9e64da0537456",
+    "url": "/homework21/static/js/main.3a9c28d5.chunk.js"
+  },
+  {
+    "revision": "6a738cf4d894f9bb7de3",
+    "url": "/homework21/static/js/runtime-main.5b7778cc.js"
+  },
+  {
+    "revision": "90679e0cfe8c59c45c061a82c48f01d4",
+    "url": "/homework21/static/media/Roboto-Regular.90679e0c.ttf"
+  }
+]);
